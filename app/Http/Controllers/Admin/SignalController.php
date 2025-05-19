b1510247adcc2fc607c1c7791a7eee06a3605acd
Lwelly Bah <lwellyebah@gmail.com>
2025-05-19 17:23:43 +0000

    j'ai corrige la fonction updateSignal en SignalController

--- app/Http/Controllers/Admin/SignalController.php
+++ app/Http/Controllers/Admin/SignalController.php
@@ -8,17 +8,17 @@ use App\Models\User;
 
 class SignalController extends Controller
 {
-   public function updateSignal($id)
-{
-    $user = User::find($id);
-
-    if (!$user) {
-        return response()->json(['message' => 'Plainte non trouvée'], 404);
+    public function updateSignal(Request $request, $id)
+    {
+        $user = User::find($id);
+    
+        if (!$user) {
+            return response()->json(['message' => 'Utilisateur non trouvé'], 404);
+        }
+    
+        $user->signal = $request->signal ?? 0;
+        $user->save();
+    
+        return response()->json(['message' => 'Signal mis à jour', 'user' => $user], 200);
     }
-
-    $user->signal = 1; // Mettre à jour signal
-    $user->save();
-
-    return response()->json(['message' => 'Signal mis à jour', 'plainte' => $plainte], 200);
-}
 }

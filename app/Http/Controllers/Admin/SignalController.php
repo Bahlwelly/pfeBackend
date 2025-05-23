24d6fb47a040b18919b01bc10fd88aa2a89f95d1
Lwelly Bah <lwellyebah@gmail.com>
2025-05-23 06:57:56 +0000

    complete the signal controller

--- app/Http/Controllers/Admin/SignalController.php
+++ app/Http/Controllers/Admin/SignalController.php
@@ -9,17 +9,24 @@ use App\Models\User;
 
 class SignalController extends Controller
 {
-   public function updateSignal($id)
-{
-    $user = User::find($id);
+    public function updateSignal(Request $request, $id)
+    {
+        $user = User::find($id);
+    
+        if (!$user) {
+            return response()->json(['message' => 'Utilisateur non trouvé'], 404);
+        }
+    
+        $user->signal = $request->signal ?? 0;
+        $user->save();
 
-    if (!$user) {
-        return response()->json(['message' => 'Plainte non trouvée'], 404);
-    }
-
-    $user->signal = 1; // Mettre à jour signal
-    $user->save();
-
-    return response()->json(['message' => 'Signal mis à jour', 'plainte' => $plainte], 200);
+        if ($user->signal >=3) {
+           $user->blocquee="blocquee";
+           $user->signal=0;
+           $user->save();
+          return response()->json(['message' => 'citoyen blocquee'], 200);
+        }
+    
+        return response()->json(['message' => 'Signal mis à jour', 'user' => $user], 200);
 }
 }
